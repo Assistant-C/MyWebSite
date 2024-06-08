0b14688bd3bdc27529e026b8c4fdf957a2e9ba1c
Assistant-C <50529706+Assistant-C@users.noreply.github.com>
2024-06-08 13:12:53 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,402 @@
+function line1(){
+    let line1 = document.getElementById("line1")
+    let lp1 = document.getElementById("lp1")
+    let svgPlus1 = document.getElementById("svgPlus1")
+    let svgMinus1 = document.getElementById("svgMinus1")
+    let line2 = document.getElementById("line2")
+    let line3 = document.getElementById("line3")
+    let line4 = document.getElementById("line4")
+    let line5 = document.getElementById("line5")
+    let lp2 = document.getElementById("lp2")
+    let lp3 = document.getElementById("lp3")
+    let lp4 = document.getElementById("lp4")
+    let lp5 = document.getElementById("lp5")
+    let svgPlus2 = document.getElementById("svgPlus2")
+    let svgPlus3 = document.getElementById("svgPlus3")
+    let svgPlus4 = document.getElementById("svgPlus4")
+    let svgPlus5 = document.getElementById("svgPlus5")
+    let svgMinus2 = document.getElementById("svgMinus2")
+    let svgMinus3 = document.getElementById("svgMinus3")
+    let svgMinus4 = document.getElementById("svgMinus4")
+    let svgMinus5 = document.getElementById("svgMinus5")
+    line1.style.height="150px"
+    lp1.style.display="block"
+    svgPlus1.style.display="none"
+    svgMinus1.style.display="block"
+    line2.style.height="60px"
+    line3.style.height="60px"
+    line4.style.height="60px"
+    line5.style.height="60px"
+    lp2.style.display="none"
+    lp3.style.display="none"
+    lp4.style.display="none"
+    lp5.style.display="none"
+    svgPlus2.style.display="block"
+    svgPlus3.style.display="block"
+    svgPlus4.style.display="block"
+    svgPlus5.style.display="block"
+    svgMinus2.style.display="none"
+    svgMinus3.style.display="none"
+    svgMinus4.style.display="none"
+    svgMinus5.style.display="none"
+}
+function line2(){
+    let line2 = document.getElementById("line2")
+    let lp2 = document.getElementById("lp2")
+    let svgPlus2 = document.getElementById("svgPlus2")
+    let svgMinus2 = document.getElementById("svgMinus2")
+    let line1 = document.getElementById("line1")
+    let line3 = document.getElementById("line3")
+    let line4 = document.getElementById("line4")
+    let line5 = document.getElementById("line5")
+    let lp1 = document.getElementById("lp1")
+    let lp3 = document.getElementById("lp3")
+    let lp4 = document.getElementById("lp4")
+    let lp5 = document.getElementById("lp5")
+    let svgPlus1 = document.getElementById("svgPlus1")
+    let svgPlus3 = document.getElementById("svgPlus3")
+    let svgPlus4 = document.getElementById("svgPlus4")
+    let svgPlus5 = document.getElementById("svgPlus5")
+    let svgMinus1 = document.getElementById("svgMinus1")
+    let svgMinus3 = document.getElementById("svgMinus3")
+    let svgMinus4 = document.getElementById("svgMinus4")
+    let svgMinus5 = document.getElementById("svgMinus5")
+    line2.style.height="150px"
+    lp2.style.display="block"
+    svgPlus2.style.display="none"
+    svgMinus2.style.display="block"
+    line1.style.height="60px"
+    line3.style.height="60px"
+    line4.style.height="60px"
+    line5.style.height="60px"
+    lp1.style.display="none"
+    lp3.style.display="none"
+    lp4.style.display="none"
+    lp5.style.display="none"
+    svgPlus1.style.display="block"
+    svgPlus3.style.display="block"
+    svgPlus4.style.display="block"
+    svgPlus5.style.display="block"
+    svgMinus1.style.display="none"
+    svgMinus3.style.display="none"
+    svgMinus4.style.display="none"
+    svgMinus5.style.display="none"
+}
+function line3(){
+    let line3 = document.getElementById("line3")
+    let lp3 = document.getElementById("lp3")
+    let svgPlus3 = document.getElementById("svgPlus3")
+    let svgMinus3 = document.getElementById("svgMinus3")
+    let line1 = document.getElementById("line1")
+    let line2 = document.getElementById("line2")
+    let line4 = document.getElementById("line4")
+    let line5 = document.getElementById("line5")
+    let lp1 = document.getElementById("lp1")
+    let lp2 = document.getElementById("lp2")
+    let lp4 = document.getElementById("lp4")
+    let lp5 = document.getElementById("lp5")
+    let svgPlus1 = document.getElementById("svgPlus1")
+    let svgPlus2 = document.getElementById("svgPlus2")
+    let svgPlus4 = document.getElementById("svgPlus4")
+    let svgPlus5 = document.getElementById("svgPlus5")
+    let svgMinus1 = document.getElementById("svgMinus1")
+    let svgMinus2 = document.getElementById("svgMinus2")
+    let svgMinus4 = document.getElementById("svgMinus4")
+    let svgMinus5 = document.getElementById("svgMinus5")
+    line3.style.height="150px"
+    lp3.style.display="block"
+    svgPlus3.style.display="none"
+    svgMinus3.style.display="block"
+    line1.style.height="60px"
+    line2.style.height="60px"
+    line4.style.height="60px"
+    line5.style.height="60px"
+    lp1.style.display="none"
+    lp2.style.display="none"
+    lp4.style.display="none"
+    lp5.style.display="none"
+    svgPlus1.style.display="block"
+    svgPlus2.style.display="block"
+    svgPlus4.style.display="block"
+    svgPlus5.style.display="block"
+    svgMinus1.style.display="none"
+    svgMinus2.style.display="none"
+    svgMinus4.style.display="none"
+    svgMinus5.style.display="none"
+}
+function line4(){
+    let line4 = document.getElementById("line4")
+    let lp4 = document.getElementById("lp4")
+    let svgPlus4 = document.getElementById("svgPlus4")
+    let svgMinus4 = document.getElementById("svgMinus4")
+    let line1 = document.getElementById("line1")
+    let line2 = document.getElementById("line2")
+    let line3 = document.getElementById("line3")
+    let line5 = document.getElementById("line5")
+    let lp1 = document.getElementById("lp1")
+    let lp2 = document.getElementById("lp2")
+    let lp3 = document.getElementById("lp3")
+    let lp5 = document.getElementById("lp5")
+    let svgPlus1 = document.getElementById("svgPlus1")
+    let svgPlus2 = document.getElementById("svgPlus2")
+    let svgPlus3 = document.getElementById("svgPlus3")
+    let svgPlus5 = document.getElementById("svgPlus5")
+    let svgMinus1 = document.getElementById("svgMinus1")
+    let svgMinus2 = document.getElementById("svgMinus2")
+    let svgMinus3 = document.getElementById("svgMinus3")
+    let svgMinus5 = document.getElementById("svgMinus5")
+    line4.style.height="150px"
+    lp4.style.display="block"
+    svgPlus4.style.display="none"
+    svgMinus4.style.display="block"
+    line1.style.height="60px"
+    line2.style.height="60px"
+    line3.style.height="60px"
+    line5.style.height="60px"
+    lp1.style.display="none"
+    lp2.style.display="none"
+    lp3.style.display="none"
+    lp5.style.display="none"
+    svgPlus1.style.display="block"
+    svgPlus2.style.display="block"
+    svgPlus3.style.display="block"
+    svgPlus5.style.display="block"
+    svgMinus1.style.display="none"
+    svgMinus2.style.display="none"
+    svgMinus3.style.display="none"
+    svgMinus5.style.display="none"
+}
+function line5(){
+    let line5 = document.getElementById("line5")
+    let lp5 = document.getElementById("lp5")
+    let svgPlus5 = document.getElementById("svgPlus5")
+    let svgMinus5 = document.getElementById("svgMinus5")
+    let line1 = document.getElementById("line1")
+    let line2 = document.getElementById("line2")
+    let line3 = document.getElementById("line3")
+    let line4 = document.getElementById("line4")
+    let lp1 = document.getElementById("lp1")
+    let lp2 = document.getElementById("lp2")
+    let lp3 = document.getElementById("lp3")
+    let lp4 = document.getElementById("lp4")
+    let svgPlus1 = document.getElementById("svgPlus1")
+    let svgPlus2 = document.getElementById("svgPlus2")
+    let svgPlus3 = document.getElementById("svgPlus3")
+    let svgPlus4 = document.getElementById("svgPlus4")
+    let svgMinus1 = document.getElementById("svgMinus1")
+    let svgMinus2 = document.getElementById("svgMinus2")
+    let svgMinus3 = document.getElementById("svgMinus3")
+    let svgMinus4 = document.getElementById("svgMinus4")
+    line5.style.height="150px"
+    lp5.style.display="block"
+    svgPlus5.style.display="none"
+    svgMinus5.style.display="block"
+    line1.style.height="60px"
+    line2.style.height="60px"
+    line3.style.height="60px"
+    line4.style.height="60px"
+    lp1.style.display="none"
+    lp2.style.display="none"
+    lp3.style.display="none"
+    lp4.style.display="none"
+    svgPlus1.style.display="block"
+    svgPlus2.style.display="block"
+    svgPlus3.style.display="block"
+    svgPlus4.style.display="block"
+    svgMinus1.style.display="none"
+    svgMinus2.style.display="none"
+    svgMinus3.style.display="none"
+    svgMinus4.style.display="none"
+}
+function show(){
+    let main = document.getElementById("main")
+    let mB = document.getElementById("mB")
+    let mBtn1 = document.getElementById("mBtn1")
+    let mBtn2 = document.getElementById("mBtn2")
+    main.style.height="1400px"
+    mB.style.display="flex"
+    mBtn1.style.display="none"
+    mBtn2.style.display="block"
+}
+function hide(){
+    let main = document.getElementById("main")
+    let mB = document.getElementById("mB")
+    let mBtn1 = document.getElementById("mBtn1")
+    let mBtn2 = document.getElementById("mBtn2")
+    main.style.height="800px"
+    mB.style.display="none"
+    mBtn2.style.display="none"
+    mBtn1.style.display="block"
+}
+
+function sub(){
+    let emInp = document.getElementById("emInp")
+    let thx1 = document.getElementById("thx1")
+    let thx2 = document.getElementById("thx2")
+    if (emInp.value==""){
+        thx1.style.display="none"
+        thx2.style.display="flex"
+        setTimeout(()=>{
+            thx2.style.display="none"
+        },3000)
+    }
+    else{
+        emInp.value=""
+        thx1.style.display="flex"
+        setTimeout(()=>{
+            thx1.style.display="none"
+        },3000)
+    }
+}
+let tem="light"
+function fun1(){
+    let dark=document.getElementById('logo')
+    box1.style.background="#252641"
+    b3_h.style.color="white"
+    b3_h1.style.color="white"
+    b3_h2.style.color="white"
+    box2.style.background="#252641"
+    box2.style.width="100%"
+    tools.style.background="#252641"
+    logo.style.rotate="180deg"
+    p2.style.color="white"
+    p3.style.color="white"
+    p4.style.color="white"
+    p5.style.color="white"
+    p6.style.color="white"
+    p7.style.color="white"
+    b2_hh1.style.color="grey"
+    box.style.background="#252641"
+    text.style.color="white"
+    h1.style.color="gray"
+    h2.style.color="white"
+    lox.style.color="white"
+    lox2.style.color="white"
+    lox3.style.color="white"
+    lox4.style.color="white"
+    lox5x.style.color="white"
+    if(tem=="light"){
+    logo.style.rotate="180deg"
+    box1.style.background="#252641"
+    b3_h.style.color="white"
+    b3_h1.style.color="white"
+    b3_h2.style.color="white"
+    box2.style.background="#252641"
+    box2.style.width="100%"
+    tools.style.background="#252641"
+    logo.style.rotate="180deg"
+    tem="dark"
+    }
+    else{
+        tem="light"
+        logo.style.rotate="360deg"
+    }
+}
+
+function test1(){
+    tB1.style.display="flex"
+    tB2.style.display="none"
+    // tB3.style.display="none"
+    // tB4.style.display="none"
+    tBl1.style.background="#31D9AB"
+    tBl2.style.background="#fff"
+    tBl3.style.background="#fff"
+    tBl4.style.background="#fff"
+}
+function answer1(){
+    let mark = 0
+    let answer = ["apple", "refrigerator", "cat", "Earth", "banana", "rainbow", "butterfly", "flower", "orange", "car", "phone", "clock", "lamp", "laptop"]
+    for (let i = 0; i<=13; i++){
+        let bBInp0 = document.getElementById(`bBInp${i+1}`).value
+        if (bBInp0 == answer[i]){
+            mark = mark + 1
+            let bBInp = document.getElementById(`bBInp${i+1}`)
+            bBInp.style.background="#61df6c"
+        }
+        else{
+            let bBInp = document.getElementById(`bBInp${i+1}`)
+            bBInp.style.background="#e94b4b"
+        }
+    }
+}
+function answer2(){
+    let mark = 0
+    let answer = ["apple", "refrigerator", "cat", "Earth", "banana", "rainbow", "butterfly", "flower", "orange", "car", "phone", "clock", "lamp", "laptop", "melon", "book"]
+    for (let i = 0; i<=14; i++){
+        let bB1Inp0 = document.getElementById(`bB1Inp${i+1}`).value
+        if (bB1Inp0 == answer[i]){
+            mark = mark + 1
+            let bBInp1 = document.getElementById(`bB1Inp${i+1}`)
+            bBInp1.style.background="#61df6c"
+        }
+        else{
+            let bBInp1 = document.getElementById(`bB1Inp${i+1}`)
+            bBInp1.style.background="#e94b4b"
+        }
+    }
+}
+function test2(){
+    tB1.style.display="none"
+    tB2.style.display="flex"
+    // tB3.style.display="none"
+    // tB4.style.display="none"
+    tBl1.style.background="#fff"
+    tBl2.style.background="#31D9AB"
+    tBl3.style.background="#fff"
+    tBl4.style.background="#fff"
+}
+const draggables = document.querySelectorAll('.ditem');
+const dropzones = document.querySelectorAll('.dropzone');
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging');
+    });
+
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging');
+    });
+});
+
+dropzones.forEach(dropzone => {
+    dropzone.addEventListener('dragover', e => {
+        e.preventDefault();
+        dropzone.classList.add('over');
+    });
+
+    dropzone.addEventListener('dragleave', () => {
+        dropzone.classList.remove('over');
+    });
+
+    dropzone.addEventListener('drop', e => {
+        e.preventDefault();
+        const draggable = document.querySelector('.dragging');
+        dropzone.appendChild(draggable);
+        dropzone.classList.remove('over');
+    });
+});
+
+function checkAnswers() {
+    const customerSays = document.getElementById('customer-says').children;
+    const supplierSays = document.getElementById('supplier-says').children;
+
+    const customerCorrect = ["I appreciate your help.", "You'd really be helping us.", "I need a favour.", "I promise this won't become the norm."];
+    const supplierCorrect = ["I think we can make an exception this time.", "I'm happy to help you.", "I'm not sure if I can do that.", "Let me see what I can do."];
+
+    let customerCorrectCount = 0;
+    let supplierCorrectCount = 0;
+
+    for (let i = 0; i < customerSays.length; i++) {
+        if (customerCorrect.includes(customerSays[i].textContent)) {
+            customerCorrectCount++;
+        }
+    }
+
+    for (let i = 0; i < supplierSays.length; i++) {
+        if (supplierCorrect.includes(supplierSays[i].textContent)) {
+            supplierCorrectCount++;
+        }
+    }
+
+    alert(`Customer correct answers: ${customerCorrectCount} out of ${customerCorrect.length}\nSupplier correct answers: ${supplierCorrectCount} out of ${supplierCorrect.length}`);
+}
